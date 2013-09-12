81403ea50a4c7e993602f8d48f862b5a95db110d
gescandon <gescandon@appirio.com>
2013-09-12 17:19:34 -0600

    add poster

---
+++ src/main/java/com/appirio/PullRequestServlet.java
@@ -0,0 +1,52 @@
+package com.appirio;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class PullRequestServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("Thank you for your interest.");
+        out.close();    	
+    }
+    
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	System.out.println("### pullrequest POST ###");
+    	StringBuilder sb = new StringBuilder();
+        try {
+            BufferedReader reader = req.getReader();
+            reader.mark(10000);
+
+            String line;
+            do {
+                line = reader.readLine();
+                sb.append(line).append("\n");
+            } while (line != null);
+            reader.reset();
+            // do NOT close the reader here, or you won't be able to get the post data twice
+        } catch(IOException e) {
+            System.out.println("getPostData couldn't.. get the post data:: " + e.getMessage());  // This has happened if the request's reader is closed    
+        }
+
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println(sb);
+        out.close();
+    }
+    
+}
