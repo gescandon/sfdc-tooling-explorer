6b56bb317462cd3808cc147142976411695da6cd
gescandon <gescandon@appirio.com>
2013-09-14 15:57:20 -0600

    fix raw tooling pre

--- src/main/webapp/js/tooling.js
+++ src/main/webapp/js/tooling.js
@@ -31,7 +31,7 @@ function toolingResp(response, objectName) {
     }
   }  
   if ($("#tooling").length > 0) {
-    $("#tooling").html( response );
+    $("#tooling").html("<pre>" + JSON.stringify($.parseJSON( response)) + "</pre>" );
   }
 
   $("#tooling-menu").html('');
