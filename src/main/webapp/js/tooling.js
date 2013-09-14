c8de39046b16bf65bc3a7fbd48ae0796cd310778
gescandon <gescandon@appirio.com>
2013-09-14 15:41:29 -0600

    icicle for mock only, for now

--- src/main/webapp/js/tooling.js
+++ src/main/webapp/js/tooling.js
@@ -23,13 +23,13 @@ function reduce(fn, a, init)
 function toolingResp(response, objectName) {
   if (isMock) {
     // replace response with mock data
-	$("#mock-response").html("Mock response: " + response);
+  $("#mock-response").html("Mock response: " + response);
     response = mockTools;
 
     if (objectName == null) {
-        response = toolingAll;    	
+        response = toolingAll;      
     }
-  }	
+  }  
   if ($("#tooling").length > 0) {
     $("#tooling").html( response );
   }
@@ -37,92 +37,92 @@ function toolingResp(response, objectName) {
   $("#tooling-menu").html('');
   var result = 'No result';
   if (objectName == null) {
-	  result = loadMenu(response);
+    result = loadMenu(response);
   } else {
-	  result = loadTools(response);
+    result = loadTools(response);
   }
   if ($("#tooling-menu").length > 0) {
-	    $("#tooling-menu").html(result);
-}	 
+      $("#tooling-menu").html(result);
+  }   
   
 }
 
 function loadMenu(response) {
-	  var rObj = $.parseJSON(response);
-	  var menu = rObj.sobjects;
-	  
-	  return reduce (function(s, x){
-	    return s + "<li><div class=\"menu-link\" onclick=\"getTooling(\'" + x.name + "\')\">" + x.name  + "</div></li>";}, menu, "<ul>") + "</ul>";
-	}
+    var rObj = $.parseJSON(response);
+    var menu = rObj.sobjects;
+    
+    return reduce (function(s, x){
+      return s + "<li><div class=\"menu-link\" onclick=\"getTooling(\'" + x.name + "\')\">" + x.name  + "</div></li>";}, menu, "<ul>") + "</ul>";
+  }
 
 function loadTools(response, objectName) {
-	  var rObj = $.parseJSON(response);
-	  var records = rObj.records;
-	  return reduce (function(s, x){
-	    return s + "<li><div class=\"menu-link\" onclick=\"getTooling(\'" + x.attributes.type + "\',\'" + x.Name + "\')\">" + x.Name  + "</div></li>";}, records, "<ul>") + "</ul>";
-	}
+    var rObj = $.parseJSON(response);
+    var records = rObj.records;
+    return reduce (function(s, x){
+      return s + "<li><div class=\"menu-link\" onclick=\"getTooling(\'" + x.attributes.type + "\',\'" + x.Name + "\')\">" + x.Name  + "</div></li>";}, records, "<ul>") + "</ul>";
+  }
 
 function getTooling(objectName,recordName) {
-	
-	var url = '/tooling?objectName=' + objectName;
-	url += recordName == null ? '' : '&recordName=' + recordName
-	$.get( url, function( response ) {
-		if (isMock) {
-		     // replace response with mock data
-		      $("#mock-response").html("Mock response: " + response);
-		      response = mockTools;
-		    }	
-			toolingResp(response, objectName);
-		});
+  
+  var url = '/tooling?objectName=' + objectName;
+  url += recordName == null ? '' : '&recordName=' + recordName
+  $.get( url, function( response ) {
+    if (isMock) {
+         // replace response with mock data
+          $("#mock-response").html("Mock response: " + response);
+          response = mockTools;
+        }  
+      toolingResp(response, objectName);
+    });
 }
 
 function explore() {
     var explorePath = $('#tool-path').val();
     
-	$.get( '/tooling?explorePath=' + explorePath, function( response ) {
-		if (isMock) {
-		     // replace response with mock data
-		      $("#mock-response").html("Mock response: " + response);
-		      response = mockTools;
-		    }	
-			$("#explore-out").html(JSON.stringify($.parseJSON(response), null, " "));
-			sendToIcicle($.parseJSON(response));
-		});
+  $.get( '/tooling?explorePath=' + explorePath, function( response ) {
+    if (isMock) {
+         // replace response with mock data
+          $("#mock-response").html("Mock response: " + response);
+          response = mockTools;
+          sendToIcicle($.parseJSON(response));
+        }  
+      $("#explore-out").html("<pre>" + JSON.stringify($.parseJSON(response), null, " ") + "</pre>");
+    });
 }
 
 function sendToIcicle(jsonr) {
-	var icicle = {
-	  name : jsonr.entityTypeName,
-	  id : 'node0',
-	  data : {
-		  size:jsonr.size}
-	  }
-	
-	var records = jsonr.records;
-	map(function(x) {
-		  var newx = {
-		    id : x.Id,
-		    name : x.Name,
-		    data :{}
-		  };
-		  for (var key in x.attributes) {
-			  if (x.attributes.hasOwnProperty(key)) {
-				  newx.data[key] = x.attributes[key];
-			  }
-			}
-		  return newx;
-	    }, records);
-	icicle.children=records;	  
-	$("#explore-out").html("<PRE>" + JSON.stringify(icicle, null, "  ") + "</PRE>");
+  var icicle = {
+    name : jsonr.entityTypeName,
+    id : 'node0',
+    data : {
+      size:jsonr.size}
+    }
+  
+  var records = jsonr.records;
+  map(function(x) {
+      var newx = {
+        id : x.Id,
+        name : x.Name,
+        data :{}
+      };
+      for (var key in x.attributes) {
+        if (x.attributes.hasOwnProperty(key)) {
+          newx.data[key] = x.attributes[key];
+        }
+      }
+      return newx;
+      }, records);
+  icicle.children=records;    
+  $("#explore-out").html("<PRE>" + JSON.stringify(icicle, null, "  ") + "</PRE>");
 
-	var json = {  
-			  "id": "aUniqueIdentifier",  
-			  "name": "usually a nodes name",  
-			  "data": {  
-			    "some key": "some value",  
-			    "some other key": "some other value"  
-			   }, };
+  var json = {  
+        "id": "aUniqueIdentifier",  
+        "name": "usually a nodes name",  
+        "data": {  
+          "some key": "some value",  
+          "some other key": "some other value"  
+         }, };
     icicleInit(icicle);
 };
-	  
+    
 
