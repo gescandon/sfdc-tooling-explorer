c9643fd1ea1d5df8605e93d6c05661e1795afed3
gescandon <gescandon@appirio.com>
2015-04-15 00:19:38 -0700

    spaces!

--- src/main/java/com/appirio/SFDCToolingManager.java
+++ src/main/java/com/appirio/SFDCToolingManager.java
@@ -11,7 +11,7 @@ import org.apache.commons.httpclient.methods.GetMethod;
 public class SFDCToolingManager {
 	  
 		public static String endpoint = "https://na12.salesforce.com";
-		public static String hostname = endpoint + "/services/data/v28.0/tooling";
+		public static String hostname = endpoint + "/services/data/v30.0/tooling";
 
 	    public static String getQuery(String objectName, String recordName) {
 	    	System.out.println(objectName + " " + recordName);
@@ -25,9 +25,9 @@ public class SFDCToolingManager {
 	    
 	    public static String explore(String explorePath, String sessionId) throws ServletException, IOException{
 	    	String query = hostname + "/" + explorePath;
-	    	System.out.println(query);
+	    	System.out.println("*** explore query : " + query);
 	        HttpClient httpclient = new HttpClient();
-	        GetMethod get = new GetMethod(query);
+	        GetMethod get = new GetMethod(query.replaceAll(" ", "+"));
 	        
 	        get.setRequestHeader("Authorization", "Bearer " + sessionId);
 	        get.setRequestHeader("Content-Type", "application/json");
@@ -67,27 +67,4 @@ public class SFDCToolingManager {
 	          }
 	          return result;
 	    }
-	    /*
-	    public static String getTooling(String objectName, String recordName, String sessionId) throws ServletException, IOException{
-	    	String query = getQuery(objectName, recordName);
-	        HttpClient httpclient = new HttpClient();
-	        GetMethod get = new GetMethod(query);
-	        
-	        get.setRequestHeader("Authorization", "Bearer " + sessionId);
-	        get.setRequestHeader("Content-Type", "application/json");
-	        String result = "No result";
-
-	          try {
-	              httpclient.executeMethod(get);
-	              result = get.getResponseBodyAsString();
-	          } catch (HttpException e) {
-	              e.printStackTrace();
-	              throw new ServletException(e);
-	          }
-	          finally {
-	              get.releaseConnection();
-	          }
-	          return result;
-	    }
-	    */
 }
